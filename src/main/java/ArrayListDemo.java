3204892b36666450a050ecf7d964fa82923bc2df
tchodzia <tchodzia@gmail.com>
2022-07-04 19:43:39 +0200

    Second commit - ArrayList Demo

---
+++ src/main/java/ArrayListDemo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Leon");
+        names.add("Olaf");
+        names.add("Dorota");
+        names.add("Monika");
+        names.add("Szymon");
+
+        System.out.println(names);
+        names.set(3, "Barbara");
+        names.add(2, "Jerzy");
+
+        System.out.println(names);
+        names.remove("Barbara");
+        System.out.println(names);
+
+
+    }
+}
