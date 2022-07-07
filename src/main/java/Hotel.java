ee4498e50495586b56f7c425b3342b05d97620ac
tchodzia <tchodzia@gmail.com>
2022-07-08 00:08:53 +0200

    Classes: Hotel, HotelDemo, Room, UserService are created with corresponding methods.

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    ArrayList<Room> rooms = new ArrayList<>();
+    ArrayList<Room> availableRooms = new ArrayList<>();
+
+    public Hotel() {
+        rooms.add(new Room(1, 4, true, true));
+        rooms.add(new Room(2, 4, true, true));
+        rooms.add(new Room(3, 4, true, true));
+        rooms.add(new Room(4, 1, false, true));
+        rooms.add(new Room(5, 1, true, true));
+        rooms.add(new Room(6, 1, true, true));
+        rooms.add(new Room(7, 1, false, true));
+        rooms.add(new Room(8, 2, true, true));
+        rooms.add(new Room(10, 2, false, true));
+        rooms.add(new Room(11, 2, true, true));
+        rooms.add(new Room(12, 2, false, false));
+        rooms.add(new Room(13, 2, true, false));
+        rooms.add(new Room(14, 3, true, true));
+        rooms.add(new Room(15, 3, true, true));
+        rooms.add(new Room(20, 3, false, false));
+        rooms.add(new Room(21, 3, true, true));
+        rooms.add(new Room(22, 4, true, false));
+        rooms.add(new Room(23, 4, true, true));
+        rooms.add(new Room(24, 4, true, true));
+        rooms.add(new Room(30, 8, true, false));
+
+        for(Room room : rooms) {
+            if (room.isAvailable()) {
+                this.availableRooms.add(room);
+            }
+        }
+    }
+
+    public void showAllRoomList() {
+        for(Room room : rooms) {
+            System.out.println(room);
+        }
+    }
 }
--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
 public class UserService {
+    ArrayList<Room> allRooms = new ArrayList<>();
+    ArrayList<Room> availableRooms = new ArrayList<>();
+
+    public UserService(ArrayList<Room> allRooms, ArrayList<Room> availableRooms) {
+        this.allRooms = allRooms;
+        this.availableRooms = availableRooms;
+    }
+
+    public boolean bookRoom(int roomNumber) {
+        for(Room availableRoom : this.availableRooms) {
+            if (availableRoom.getRoomNumber() == roomNumber && availableRoom.isAvailable()) {
+                this.availableRooms.remove(availableRoom);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkOutFromHotel(int roomNumber) {
+        for(Room room : this.allRooms) {
+            if (room.getRoomNumber() == roomNumber && !room.isAvailable()) {
+                this.availableRooms.add(room);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void showAllRoomList() {
+        for(Room room : allRooms) {
+            System.out.println(room);
+        }
+    }
+
+    public void showAvailableRoomList() {
+        for(Room room : availableRooms) {
+            System.out.println(room);
+        }
+    }
+
 }
