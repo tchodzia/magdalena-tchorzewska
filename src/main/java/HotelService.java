6a667f4d0790aea566d0d767de8b62e2209cf629
tchodzia <tchodzia@gmail.com>
2022-07-08 00:08:53 +0200

    Classes: Hotel, HotelDemo, Room, UserService are created with corresponding methods.

---
+++ src/main/java/HotelService.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class HotelDemo {
+    public static void main(String[] args) {
+        Hotel hotel = new Hotel();
+
+        UserService userService = new UserService(hotel.rooms, hotel.availableRooms);
+       /*
+        System.out.println("Show all room list: ");
+        userService.showAllRoomList();
+        System.out.println("Show available rooms: ");
+        //userService.showAvailableRoomList();
+        */
+        userService.bookRoom(4);
+        userService.bookRoom(12);
+        userService.bookRoom(13);
+        userService.bookRoom(30);
+        //userService.showAvailableRoomList();
+
+        //userService.checkOutFromHotel(5);
+        userService.checkOutFromHotel(13);
+        userService.checkOutFromHotel(30);
+        //userService.showAvailableRoomList();
+
+        boolean run = true;
+        int roomNumber;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Choose option for Hotel App.");
+            System.out.println(
+                    "Select an option: \n" +
+                            "  1) Book room \n" +
+                            "  2) Check-out \n" +
+                            "  3) Show available rooms \n" +
+                            "  4) Show all rooms \n" +
+                            "  5) Exit\n "
+            );
+            int selection = scanner.nextInt();
+
+            switch (selection) {
+                case 1:
+                    System.out.println("Give room number to check-in: ");
+                    roomNumber = scanner.nextInt();
+                    userService.bookRoom(roomNumber);
+                    break;
+                case 2:
+                    System.out.println("Give room number to check-out: ");
+                    roomNumber = scanner.nextInt();
+                    userService.checkOutFromHotel(roomNumber);
+                    break;
+                case 3:
+                    userService.showAvailableRoomList();
+                    break;
+                case 4:
+                    userService.showAllRoomList();
+                    break;
+                case 5:
+                default:
+                    run = false;
+                    break;
+            }
+        } while (run);
+    }
+
+
+}
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,31 @@
 public class Room {
+    private int roomNumber;
+    private int roomSize;
+    private boolean isBathroom;
+    private boolean isAvailable;
+
+    public Room(int roomNumber, int roomSize, boolean isBathroom, boolean isAvailable) {
+        this.roomNumber = roomNumber;
+        this.roomSize = roomSize;
+        this.isBathroom = isBathroom;
+        this.isAvailable = isAvailable;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber=" + roomNumber +
+                ", roomSize=" + roomSize +
+                ", isBathroom=" + isBathroom +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
 }
